dc1611bd9399694662f6e02f1e6e02a726f5aae3
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2023-05-12 16:17:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@phr.wy7ryxx.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const appointmentOptionCollection = client.db("patientReportHub").collection("appointmentOption");
+
+    app.get("/appointmentOptions", async (req, res) => {
+      const query = {};
+      const options = await appointmentOptionCollection.find(query).toArray();
+      res.send(options);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("doctor is running");
+});
+
+app.listen(port, () => {
+  console.log("run doctor run");
+});
